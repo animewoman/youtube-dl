5d76c5174db580f4fcc4470c25770f12d8acf112
animewoman <bekameilun@gmail.com>
2019-04-11 19:37:55 +0600

    added normal history

--- converter/models.py
+++ converter/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class GetFiles(models.Model):
     name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date')
 
--- converter/views.py
+++ converter/views.py
@@ -2,26 +2,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .forms import Download
 import youtube_dl
-from django.views.static import serve
-import os
+import datetime
 from .models import GetFiles
 from django.shortcuts import HttpResponse
+from django.utils.timezone import now
 
 
 def index(request):
     if request.method == 'POST':
         form = Download(request.POST)
         if form.is_valid():
-            data = ''
             link = form.cleaned_data
             temp = link['link']
-            for x in range(len(temp)):
-                if temp[x] == '=':
-                    for n in range(x + 1, len(temp)):
-                        data += temp[n]
-                    break
-            x = GetFiles.objects.create(name=data)
-            x.save()
             ydl_opts = {
                 'keepvideo': True,
                 'format': 'bestaudio/best',
@@ -30,12 +22,16 @@ def index(request):
                     'preferredcodec': 'mp3',
                     'preferredquality': '192',
                 }],
-                'outtmpl': './audio/{}'.format(data),
+                'outtmpl': './audio/%(title)s',
             }
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([link['link']])
-            print(GetFiles.objects.values_list('name'))
-            return download(request, data)
+                meta = ydl.extract_info(
+                    temp
+                )
+
+            x = GetFiles.objects.create(name=meta['title'], pub_date=now())
+            x.save()
+            return HttpResponse('File has succesfully downloaded in audio folder of this project')
 
     else:
         form = Download()
@@ -43,10 +39,7 @@ def index(request):
     return render(request, 'home.html', {'form': form})
 
 
-def download(request, data):
-    filepath = 'audio/{}'.format(data)
-    return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
-
-
 def history(request):
-    return HttpResponse(GetFiles.objects.values_list('name'))
+    convert_to_list = GetFiles.objects.values_list('name', 'pub_date')
+    convert_to_list = list(convert_to_list)
+    return render(request, 'history.html', {'options': convert_to_list})
