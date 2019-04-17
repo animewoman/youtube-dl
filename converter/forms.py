f97445c1aaa69149b2e149a8eaf2154656e1739f
animewoman <bekameilun@gmail.com>
2019-04-17 18:43:46 +0600

    added required

--- converter/forms.py
+++ converter/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class Download(forms.Form):
-    link = forms.CharField(label='link')
-    email = forms.CharField(label='email')
+    link = forms.CharField(label='link', required=True)
+    email = forms.EmailField(label='email', required=True)
